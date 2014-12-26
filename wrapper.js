582dfe243e80da81120b62612c91c5b7f12e4664
Руслан <jokagent@gmail.com>
2014-12-26 16:22:28 +0400

    Init

---
+++ wrapper.js
@@ -0,0 +1,28 @@
+// Jade 'HTML' wrapper for SocketStream 0.3
+
+var fs = require('fs'),
+    reactTools = require('react-tools');
+
+exports.init = function(root, config) {
+
+  if (!(config && typeof(config) === 'object')) config = {};
+
+  return {
+
+    name: 'React',
+
+    extensions: ['jsx'],
+
+    assetType: 'js',
+
+    contentType: 'text/javascript',
+
+    compile: function(path, options, cb) {
+
+      var input = fs.readFileSync(path, 'utf8');
+      var output = reactTools.transform(input, {filename: path});
+
+      cb(output);
+    }
+  };
+};
